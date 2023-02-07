3cda1703344aafd4895e2a4784fc7b2e9b9c06d1
vio-ubuntu <saiviolet@gmail.com>
2023-02-07 19:50:34 +0300

    ЮНИТ ТЕСТЫ / Тестирование сортировок

---
+++ src/components/sorting-page/__tests__/sorting-page.tsx
@@ -0,0 +1,34 @@
+import {selectSortAnimations , bubbleSortAnimations} from "../ulits";
+import {nanoid} from "nanoid";
+import {ElementStates} from "../../../types/element-states";
+import {IArrayColumns} from "../../../types/components";
+
+const testArray = [12,77,3,8,17];
+const sortTestArray = [3,8,12,17,77];
+let array:IArrayColumns[] = [];
+describe('Тестирование алгоритмов сортировки выбором', () => {
+  test('Корректно сортирует массив из нескольких элементов', () => {
+    testArray.forEach(arr => array.push({number: arr, key: nanoid(10), state: ElementStates.Default}));
+    expect(selectSortAnimations(array,"ascending").array).toEqual(sortTestArray);
+  });
+  test('Корректно сортирует пустой массив', () => {
+    expect(selectSortAnimations([],"ascending").array).toEqual([]);
+  });
+  test('Корректно сортирует массив', () => {
+    const array = [{number: 22, key: nanoid(10), state: ElementStates.Default}]
+    expect(selectSortAnimations(array,"ascending").array).toEqual([22]);
+  });
+});
+describe('Тестирование алгоритмов сортировки пузырьком', () => {
+  test('Корректно сортирует массив из нескольких элементов', () => {
+    testArray.forEach(arr => array.push({number: arr, key: nanoid(10), state: ElementStates.Default}));
+    expect(bubbleSortAnimations(array,"ascending").array).toEqual(sortTestArray);
+  });
+  test('Корректно сортирует пустой массив', () => {
+    expect(bubbleSortAnimations([],"ascending").array).toEqual([]);
+  });
+  test('Корректно сортирует массив', () => {
+    const array = [{number: 22, key: nanoid(10), state: ElementStates.Default}]
+    expect(bubbleSortAnimations(array,"ascending").array).toEqual([22]);
+  });
+})

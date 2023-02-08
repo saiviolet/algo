354638b02672da240ced927c2eb68b91e5efd567
vio-ubuntu <saiviolet@gmail.com>
2023-02-08 20:30:35 +0300

    CYPRESS / Страница СТЕК - 3

--- cypress/e2e/stack.spec.cy.ts
+++ cypress/e2e/stack.spec.cy.ts
@@ -29,11 +29,11 @@ describe('Страница СТЕК', () => {
     cy.clock();
     cy.get("@input").type(TEST_STACK_VALUES[0]).should('have.value', TEST_STACK_VALUES[0]);
     cy.get("@buttonAdd").should('be.visible').click();
-    cy.get(TEST_CIRCLE).as('circle')
+    cy.get(TEST_CIRCLE).as('circle');
     cy.get('@circle')
       .should('have.text',TEST_STACK_VALUES[0])
       .and('have.css', 'border', TEST_BORDER_COLOR.Changing)
-    cy.get('@circle').prev().contains('top')
+    cy.get('@circle').prev().contains('top');
     cy.tick(SHORT_DELAY_IN_MS);
     cy.get('@circle').should('have.text',TEST_STACK_VALUES[0]).and('have.css', 'border', TEST_BORDER_COLOR.Default);
     // добавление второго элемента
@@ -68,9 +68,60 @@ describe('Страница СТЕК', () => {
       .last()
       .prev()
       .contains('top');
+  });
+  it('Удаление из стека работает корректно', () => {
+    cy.clock();
+    cy.get("@input").type(TEST_STACK_VALUES[0]).should('have.value', TEST_STACK_VALUES[0]);
+    cy.get("@buttonAdd").should('be.visible').click();
+    cy.get(TEST_CIRCLE).as('circle');
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get('@circle').should('have.text',TEST_STACK_VALUES[0]);
+    cy.get('@circle').prev().contains('top');
+    cy.get("@input").type(TEST_STACK_VALUES[1]).should('have.value', TEST_STACK_VALUES[1]);
+    cy.get("@buttonAdd").should('be.visible').click();
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get('@circle').last().should('have.text',TEST_STACK_VALUES[1]);
+    cy.get('@circle')
+      .last()
+      .prev()
+      .contains('top');
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get("@circle").should("have.length", 2);
+    cy.get("@buttonDelete").should('be.visible').click();
+    cy.get('@circle').last().should('have.text',TEST_STACK_VALUES[0]);
+    cy.get('@circle')
+      .last()
+      .prev()
+      .contains('top');
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get("@circle").should("have.length", 1);
+    cy.get("@buttonDelete").should('be.visible').click();
+    cy.get('@circle').should('not.exist');
+    cy.get("@buttonAdd").should('be.visible');
+    cy.get("@buttonDelete").should('be.visible');
+  });
+
+  it('Корректная очистка стека', () => {
+    cy.clock();
+    cy.get("@input").type(TEST_STACK_VALUES[0]).should('have.value', TEST_STACK_VALUES[0]);
+    cy.get("@buttonAdd").should('be.visible').click();
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get("@input").type(TEST_STACK_VALUES[1]).should('have.value', TEST_STACK_VALUES[1]);
+    cy.get("@buttonAdd").should('be.visible').click();
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get("@input").type(TEST_STACK_VALUES[2]).should('have.value', TEST_STACK_VALUES[2]);
+    cy.get("@buttonAdd").should('be.visible').click();
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get(TEST_CIRCLE).as('circle');
+    cy.get("@circle").should("have.length", 3);
+    cy.get("@buttonClear").should('be.visible').click();
+    cy.tick(SHORT_DELAY_IN_MS);
+    cy.get('@circle').should('not.exist');
   })
+
   afterEach(() => {
     cy.get('@input').should('be.empty');
     cy.get('@buttonAdd').should('be.disabled');
-  })
+  });
+
 });
